6a11adf9f92a0bf46ba45fc42df122dd6a4c31ee
seotory <seotory437@gmail.com>
2018-10-21 01:51:09 +0900

    add

--- src/main/codility/lesson/lesson3/FrogJump.java
+++ src/main/codility/lesson/lesson3/FrogJump.java
@@ -1,13 +1,18 @@
 package codility.lesson.lesson3;
 
 public class FrogJump {
+
+    public static void main(String[] args) {
+        System.out.println(solution(10, 85, 30));
+    }
+
     /**
      * @param X : 최초 개구리의 위치
      * @param Y : Y보다 크거나 같은 위치로 가고 싶어함
      * @param D : 개구리는 항상 고정된 D의 값으로 점프를 함
      * @return
      */
-    public int solution(int X, int Y, int D) {
+    static public int solution(int X, int Y, int D) {
         return (int)(Math.ceil(((double)Y-(double)X)/(double)D));
     }
 }
