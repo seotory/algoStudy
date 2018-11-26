98499c9cf6948a33bdd3c3e5e1c6bcc5907b1fbf
USER <user@AL01404554.local>
2018-11-26 22:37:29 +0900

    추가

---
+++ src/main/codility/lesson/lesson4/MissingInteger.java
@@ -0,0 +1,36 @@
+package codility.lesson.lesson4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MissingInteger {
+
+	static public void main(String[] args) {
+		solution(new int[]{1, 3, 6, 4, 1, 2}); // = 5
+	}
+
+	static public int solution(int[] A) {
+		Map<Integer, Boolean> map = new HashMap();
+		int max = 1;
+
+		for (int a : A) {
+			if (a > 0) {
+				map.put(a,true);
+				if (max < a) {
+					max = a;
+				}
+			}
+		}
+
+		for (int i=1 ; i <= max ; i++) {
+			if (max == i && map.containsKey(i)) {
+				return i+1;
+			}
+			if (!map.containsKey(i)){
+				return i;
+			}
+		}
+		return 1;
+	}
+
+}
